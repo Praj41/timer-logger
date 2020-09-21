8554c900856abcf007c15eae4a757eaa77f4f9ab
Praj41 <pcbhamare.41@gmail.com>
2020-09-21 17:24:22 +0530

    lap support

--- Timer.h
+++ Timer.h
@@ -20,7 +20,7 @@ public:
 
         std::cout << duration << "us(" << duration * 0.001 << "ms)-END" << std::endl;
     }
-    void step() {
+    void lap() {
         auto EndTimePoint = std::chrono::high_resolution_clock::now();
 
         auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
@@ -29,5 +29,18 @@ public:
         auto duration = end - start;
 
         std::cout << duration << "us(" << duration * 0.001 << "ms)-STEP_" << ++_step << std::endl;
+    }
+    void lap_reset() {
+        auto EndTimePoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
+        auto end= std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
+
+        auto duration = end - start;
+
+        _step = 0;
+        std::cout << duration << "us(" << duration * 0.001 << "ms)-STEP_" << _step << std::endl;
+
+        StartTimePoint = std::chrono::high_resolution_clock::now();
     }
 };
