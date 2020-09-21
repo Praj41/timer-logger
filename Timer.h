f75b341691a39e63680897530a94ad1e89ece525
Praj41 <pcbhamare.41@gmail.com>
2020-09-21 17:14:02 +0530

    init commit

---
+++ Timer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+
+class Timer {
+    std::chrono::time_point< std::chrono::high_resolution_clock> StartTimePoint;
+    unsigned int _step;
+public:
+    Timer() : _step(0) {
+        StartTimePoint = std::chrono::high_resolution_clock::now();
+    }
+    ~Timer() {
+        auto EndTimePoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
+        auto end= std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
+
+        auto duration = end - start;
+
+        std::cout << duration << "us(" << duration * 0.001 << "ms)-END" << std::endl;
+    }
+    void step() {
+        auto EndTimePoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
+        auto end= std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
+
+        auto duration = end - start;
+
+        std::cout << duration << "us(" << duration * 0.001 << "ms)-STEP_" << ++_step << std::endl;
+    }
+};
