784c974165b5b648d57454250759d76bc90148c4
Praj41 <pcbhamare.41@gmail.com>
2021-02-06 19:53:55 +0530

    lap support

---
+++ RNG.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+
+unsigned long int* RNG(unsigned int nos = 10, int min = 0, int max = 1000) {
+
+    std::random_device rd;
+
+    std::mt19937::result_type seed = rd() ^ (
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now().time_since_epoch()
+                    ).count() +
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::microseconds>(
+                            std::chrono::high_resolution_clock::now().time_since_epoch()
+                    ).count() );
+
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<unsigned> distrib(min, max);
+
+    auto *arr = new unsigned long int[nos];
+
+    for (unsigned long j = 0; j < nos; ++j)
+    {
+        arr[j] = distrib(gen);
+    }
+
+    std::cout << '\n';
+    return arr;
+}
+
+unsigned long int RNG_s(int min = 0, int max = 1000) {
+
+    std::random_device rd;
+
+    std::mt19937::result_type seed = rd() ^ (
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now().time_since_epoch()
+                    ).count() +
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::microseconds>(
+                            std::chrono::high_resolution_clock::now().time_since_epoch()
+                    ).count() );
+
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<unsigned> distrib(min, max);
+
+    std::cout << '\n';
+    return distrib(gen);
+}
--- Timer.h
+++ Timer.h
@@ -2,44 +2,45 @@
 
 #include <iostream>
 #include <chrono>
+#include <utility>
 
 class Timer {
     std::chrono::time_point< std::chrono::high_resolution_clock> StartTimePoint;
-    unsigned int _step;
+    unsigned int _step, _tag;
 public:
-    Timer() : _step(0) {
+    Timer(unsigned int tag = 0) : _step(0), _tag(tag) {
         StartTimePoint = std::chrono::high_resolution_clock::now();
     }
     ~Timer() {
         auto EndTimePoint = std::chrono::high_resolution_clock::now();
 
         auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
-        auto end= std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
 
         auto duration = end - start;
 
-        std::cout << duration << "us(" << duration * 0.001 << "ms)-END" << std::endl;
+        std::cout << duration << "us (" << duration * 0.001 << "ms) - END Timer-" << _tag << std::endl;
     }
     void lap() {
         auto EndTimePoint = std::chrono::high_resolution_clock::now();
 
         auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
-        auto end= std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
 
         auto duration = end - start;
 
-        std::cout << duration << "us(" << duration * 0.001 << "ms)-STEP_" << ++_step << std::endl;
+        std::cout << duration << "us (" << duration * 0.001 << "ms) - STEP_" << ++_step << " Timer-" << _tag << std::endl;
     }
     void lap_reset() {
         auto EndTimePoint = std::chrono::high_resolution_clock::now();
 
         auto start = std::chrono::time_point_cast<std::chrono::microseconds>(StartTimePoint).time_since_epoch().count();
-        auto end= std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(EndTimePoint).time_since_epoch().count();
 
         auto duration = end - start;
 
         _step = 0;
-        std::cout << duration << "us(" << duration * 0.001 << "ms)-STEP_" << _step << std::endl;
+        std::cout << duration << "us (" << duration * 0.001 << "ms) - RESET_STEP_" << _step << " Timer-" << _tag << std::endl;
 
         StartTimePoint = std::chrono::high_resolution_clock::now();
     }
